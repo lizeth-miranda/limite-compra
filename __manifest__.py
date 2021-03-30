c716101fce370b33b1cb08652afb62b569a6a9ff
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-03-30 08:42:29 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'limite_compra',
+    'version': '13.1',
+    'author': 'Demsa',
+    'website': '',
+    'depends': [
+        'purchase',
+
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/purchase_order.xml',
+        'views/ir_cron.xml',
+
+
+    ],
+}
